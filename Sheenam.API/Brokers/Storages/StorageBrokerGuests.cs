036bcc3b5f13b275e29a01f95c1a1c67816dd1b9
Durdonaxon95 <qilichovadurdona7@gmail.com>
2025-04-10 07:05:12 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.API/Brokers/Storages/StorageBrokerGuests.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//=================================================
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundation.Guests;
+
+namespace Sheenam.API.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests { get; set; } = null!;
+    }
+}
---
+++ Sheenam.API/Models/Foundation/Guests/Guest.cs
@@ -0,0 +1,19 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//=================================================
+
+namespace Sheenam.Api.Models.Foundation.Guests
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTimeOffset DateOfBirth { get; set; }
+        public GenderType Gender { get; set; }
+    }
+}
